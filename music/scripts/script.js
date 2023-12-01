fd407b9b78ff7a19c747eec63cf641740e54558c
SamuelCGarza <samuelcgarza21@gmail.com>
2023-12-01 21:32:28 +0000

    c11

---
+++ music/scripts/script.js
@@ -0,0 +1,17 @@
+/*  
+Author: Samuel Garza
+File Name: script.js
+Date: 11/16/2023
+*/
+
+// Hamburger menu function // 
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
+
+
